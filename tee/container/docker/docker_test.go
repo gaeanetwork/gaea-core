5db15d881ca928df47af998a3ce53cafd4db1c0f
WangFeifan <314130948@qq.com>
2019-07-11 18:45:29 +0800

    implement docker upload function

--- tee/container/docker/docker_test.go
+++ tee/container/docker/docker_test.go
@@ -1,6 +1,8 @@
 package docker
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +18,24 @@ func Test_Create(t *testing.T) {
 	err := c.Create()
 	assert.NoError(t, err)
 	assert.NotEmpty(t, c.address)
+}
+
+func Test_Upload(t *testing.T) {
+	c := New()
+	algorithm, err := ioutil.ReadFile("/home/rabbit/teetest/client/resume")
+	assert.NoError(t, err)
+	A, err := ioutil.ReadFile("/home/rabbit/teetest/A/A_resume.txt")
+	assert.NoError(t, err)
+	B, err := ioutil.ReadFile("/home/rabbit/teetest/B/B_resume.txt")
+	assert.NoError(t, err)
+	C, err := ioutil.ReadFile("/home/rabbit/teetest/C/C_resume.txt")
+	assert.NoError(t, err)
+	dataList := [][]byte{A, B, C}
+	err = c.Upload(algorithm, dataList)
+	assert.NoError(t, err)
+
+	os.Remove("main")
+	os.Remove("0")
+	os.Remove("1")
+	os.Remove("2")
 }
