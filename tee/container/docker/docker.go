caac34787fdfebb90a3e2485c799892d86aba1ee
WangFeifan <314130948@qq.com>
2019-07-20 11:53:47 +0800

    Add docker container upload implementation

--- tee/container/docker/docker.go
+++ tee/container/docker/docker.go
@@ -1,9 +1,16 @@
 package docker
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"crypto/sha256"
 	"fmt"
+	"path/filepath"
+	"strconv"
 
 	docker "github.com/fsouza/go-dockerclient"
+	"github.com/gaeanetwork/gaea-core/common"
 	"github.com/hyperledger/fabric/core/container/util"
 )
 
@@ -46,6 +53,62 @@ func Create() (*Container, error) {
 		client:    client,
 		startFunc: startFunc,
 	}, nil
+}
+
+// Upload for development
+func (c *Container) Upload(algorithm []byte, dataList [][]byte) error {
+	if len(algorithm) == 0 {
+		return fmt.Errorf("algorithm bytes is empty")
+	}
+
+	if len(dataList) == 0 {
+		return fmt.Errorf("dataList bytes is empty")
+	}
+
+	payload := bytes.NewBuffer(nil)
+	gw := gzip.NewWriter(payload)
+	tw := tar.NewWriter(gw)
+
+	// Calculate algorithm hash
+	hash := sha256.Sum256(algorithm)
+	c.algorithmHash = common.BytesToHex(hash[:])
+	c.cmd = filepath.Join(c.address, "main")
+	err := util.WriteBytesToPackage(c.cmd, algorithm, tw)
+	if err != nil {
+		return err
+	}
+
+	c.dataHash = make([]string, 0)
+	for index, data := range dataList {
+		// Calculate data hash
+		if hash = sha256.Sum256(data); err != nil {
+			return err
+		}
+		c.dataHash = append(c.dataHash, common.BytesToHex(hash[:]))
+
+		arg := filepath.Join(c.address, strconv.Itoa(index))
+		if err = util.WriteBytesToPackage(arg, data, tw); err != nil {
+			return err
+		}
+
+		c.cmd += " " + arg
+	}
+
+	// Write the tar file out
+	if err := tw.Close(); err != nil {
+		return fmt.Errorf("Error writing files to upload to Docker instance into a temporary tar blob: %s", err)
+	}
+
+	gw.Close()
+
+	c.uploadFunc = func(containerID string) error {
+		return c.client.UploadToContainer(containerID, docker.UploadToContainerOptions{
+			InputStream:          bytes.NewReader(payload.Bytes()),
+			Path:                 "/",
+			NoOverwriteDirNonDir: false,
+		})
+	}
+	return nil
 }
 
 // Destroy for development
