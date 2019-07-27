b7be4424fc16d26ca471b5c8e149a61597387992
chenhuaying <chenhuaying@sina.cn>
2019-07-27 19:07:52 +0800

    update, use a static dir for js

--- did/server/app/app.js
+++ did/server/app/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 8081
 
+app.use(express.static('public'))
+
 // serve index.html as the home page
 app.get('/', function (req, res) {
 	res.sendFile('index.html', { root: __dirname })
