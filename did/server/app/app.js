6f43f18bb0166c3ec41678def5f534daa9c09329
chenhuaying <chenhuaying@sina.cn>
2019-08-02 11:08:04 +0800

    update demo

--- did/server/app/app.js
+++ did/server/app/app.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const axios = require('axios')
 var bodyParser = require('body-parser')
+var session = require('express-session')
 
 const app = express()
 const port = 8081
 
-app.use(express.static('public'))
+app.use(express.static('public'), session({
+	secret: 'cookie-secret-key',
+	resave: false,
+	saveUninitialized: true
+}))
+.use(function (req, res, next) {
+
+	if (['/', '/login', '/verify'].indexOf(req.path) >= 0) {
+		return next()
+	}
+
+	if (!req.session.did) {
+		return res.status(401).send("Unauthorized, please log in.")
+	}
+
+	next()
+})
 
 app.all('*', function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
 	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-	res.header("Content-Type", "application/json;charset=utf-8");
+	res.header("Content-Type", "text/html; charset=utf-8");
 
 	if (req.method == 'OPTIONS') {
 		res.send(200);
@@ -23,7 +40,18 @@ app.all('*', function (req, res, next) {
 
 // serve index.html as the home page
 app.get('/', function (req, res) {
+	console.log(req.session.id)
 	res.sendFile('index.html', { root: __dirname });
+})
+
+app.get('/login', function (req, res) {
+	redirectUrl = 'http://localhost:8081/verify'
+	// XXX TODO: sign message before send
+	authRequest = {
+		client_id: redirectUrl,
+		nonce: req.session.id,
+	  }
+	  res.status(200).send(authRequest)
 })
 
 var textParser = bodyParser.text({ type: 'application/json' });
@@ -35,8 +63,10 @@ app.post('/verify', textParser, function (req, res) {
 	.then(function (response) {
 		console.log('<<<<<<<<<<:', response.data);
 		if (response.data.result == true) {
+			res.header("Content-Type", "application/json;charset=utf-8");
 			res.status(200).json({result:true});
 		} else {
+			res.header("Content-Type", "application/json;charset=utf-8");
 			res.status(200).json({result:false});
 		}
 	})
