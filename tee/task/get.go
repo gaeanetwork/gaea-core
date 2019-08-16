8399bed6d08150ff7e8602111c95a64905011e58
WangFeifan <314130948@qq.com>
2019-08-16 10:23:49 +0800

    Add GetAll tee tasks features

--- tee/task/get.go
+++ tee/task/get.go
@@ -27,3 +27,33 @@ func GetByID(id string) (*tee.Task, error) {
 
 	return &d, nil
 }
+
+// GetAll get all the tee task from the blockchain.
+func GetAll() ([]*tee.Task, error) {
+	service, err := factory.GetSmartContractService(tee.ImplementPlatform)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get smart contract service, platform: %s", tee.ImplementPlatform)
+	}
+
+	result, err := service.Query(ChaincodeName, []string{MethodGetAll})
+	if err != nil {
+		return nil, err
+	}
+
+	var dataList [][]byte
+	if err = json.Unmarshal([]byte(result), &dataList); err != nil {
+		return nil, err
+	}
+
+	dList := make([]*tee.Task, 0)
+	for _, data := range dataList {
+		var d tee.Task
+		if err = json.Unmarshal([]byte(data), &d); err != nil {
+			return nil, err
+		}
+
+		dList = append(dList, &d)
+	}
+
+	return dList, nil
+}
