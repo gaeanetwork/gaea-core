ab4f27095bae3684af3545a056fe82fa91cb20c2
WangFeifan <314130948@qq.com>
2019-08-20 16:05:53 +0800

    Add hyperledger fabric clients

---
+++ smartcontract/fabric/chaincode/cmd/factory.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	"github.com/hyperledger/fabric/msp"
+	"gitlab.com/jaderabbit/go-rabbit/common/client"
+)
+
+// ChaincodeCmdFactory holds the clients used by ChaincodeCmd
+type ChaincodeCmdFactory struct {
+	EndorserClients []*client.EndorserClient
+	DeliverClients  []*client.DeliverClient
+	Certificate     tls.Certificate
+	Signer          msp.SigningIdentity
+	BroadcastClient *client.BroadcastClient
+}
+
+// Close the EndorserClients and DeliverClients of ChaincodeCmdFactory
+func (cf *ChaincodeCmdFactory) Close() error {
+	eLen, dLen := len(cf.EndorserClients), len(cf.DeliverClients)
+	if eLen != dLen {
+		return fmt.Errorf("EndorserClients length[%d] is not equals to DeliverClients[%d]", eLen, dLen)
+	}
+
+	for index := 0; index < eLen; index++ {
+		cf.EndorserClients[index].Close()
+		cf.DeliverClients[index].Close()
+	}
+
+	if cf.BroadcastClient != nil {
+		cf.BroadcastClient.Close()
+	}
+
+	return nil
+}
