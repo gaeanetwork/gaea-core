f59b31b69acb230ecdef14c07cfe9858afe9c341
WangFeifan <314130948@qq.com>
2019-08-28 11:18:27 +0800

    Add tee task protobuf and rpc service

---
+++ services/task/task.go
@@ -0,0 +1,47 @@
+package task
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/gaeanetwork/gaea-core/protos/service"
+	"github.com/gaeanetwork/gaea-core/smartcontract"
+	"github.com/gaeanetwork/gaea-core/smartcontract/factory"
+	"github.com/gaeanetwork/gaea-core/tee"
+	"github.com/gaeanetwork/gaea-core/tee/task"
+	"github.com/pkg/errors"
+)
+
+// Service is used to do some tee tasks
+type Service struct {
+	scService smartcontract.Service
+}
+
+// NewTeeTaskService create a tee task service
+func NewTeeTaskService() *Service {
+	smartcontractService, err := factory.GetSmartContractService(tee.ImplementPlatform)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Service{smartcontractService}
+}
+
+// Create creates a tee task by sending a create transaction to the blockchain.
+func (s *Service) Create(ctx context.Context, req *service.CreateRequest) (*service.CreateResponse, error) {
+	if len(req.DataId) <= 0 {
+		return nil, errors.Errorf("data id are non-empty")
+	}
+
+	ids, err := json.Marshal(req.DataIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := s.scService.Invoke(task.ChaincodeName, []string{task.MethodCreate, string(ids), req.AlgorithmId, req.ResultAddress})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create tee task, req: %v", req)
+	}
+
+	return &service.CreateResponse{TaskId: string(result)}, nil
+}
