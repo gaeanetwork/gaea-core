af925f9e105841208693a061b2f4628533a49a4f
WTer <wesleyliu@qq.com>
2019-08-29 10:39:33 +0800

    add unittests for signpackage

---
+++ smartcontract/fabric/chaincode/signpackage_test.go
@@ -0,0 +1,25 @@
+package chaincode
+
+import (
+	"testing"
+
+	"github.com/hyperledger/fabric/peer/common"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/jaderabbit/go-rabbit/core/config"
+)
+
+func Test_SignPackage(t *testing.T) {
+	config.Initialize()
+	ReadViperConfiguration()
+	conf, err := GetConfig("system01")
+	assert.NoError(t, err)
+	assert.NotNil(t, conf)
+
+	common.InitCmd(nil, []string{})
+	common.SetOrdererEnv(nil, []string{})
+
+	conf.ChaincodeName = "user"
+
+	err = signpackage(conf)
+	assert.NoError(t, err)
+}
