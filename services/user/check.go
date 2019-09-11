53c2ed1ecbdf91261a5ffa82d24b2b97a4adb709
WangFeifan <314130948@qq.com>
2019-09-11 18:10:45 +0800

    Implement user grpc service login function

---
+++ services/user/check.go
@@ -0,0 +1,21 @@
+package user
+
+import "github.com/pkg/errors"
+
+func checkUsernameLen(username string) error {
+	len := len(username)
+	if len <= 0 || len > 32 {
+		return errors.Errorf("Invalid username length, should be (0, 32], now: %d", len)
+	}
+
+	return nil
+}
+
+func checkPasswordLen(password string) error {
+	len := len(password)
+	if len <= 0 || len > 32 {
+		return errors.Errorf("Invalid password length, should be (0, 32], now: %d", len)
+	}
+
+	return nil
+}
--- services/user/user_test.go
+++ services/user/user_test.go
@@ -3,10 +3,10 @@ package user
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/gaeanetwork/gaea-core/common"
 	pb "github.com/gaeanetwork/gaea-core/protos/user"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
 
 func Test_Register(t *testing.T) {
@@ -44,4 +44,59 @@ func Test_Register(t *testing.T) {
 	req1.UserName = common.GetRandomStringByLen(33)
 	resp, err = s.Register(nil, req1)
 	assert.Error(t, err, "Invalid password length")
+}
+
+func Test_Login(t *testing.T) {
+	s := NewUserService()
+	req := &pb.RegisterRequest{
+		UserName: "test-username",
+		Password: "test-password",
+	}
+
+	resp, err := s.Register(nil, req)
+	assert.NoError(t, err)
+
+	loginReq := &pb.LoginRequest{
+		UserName: resp.User.UserName,
+		Password: resp.User.Password,
+	}
+	loginResp, err := s.Login(nil, loginReq)
+	assert.NoError(t, err)
+	assert.Equal(t, loginResp.User, resp.User)
+
+	// Error - Invalid request username
+	req1 := &pb.LoginRequest{
+		UserName: "",
+		Password: resp.User.Password,
+	}
+	_, err = s.Login(nil, req1)
+	assert.Error(t, err, "Invalid username length")
+	req1.UserName = "resp.User.UserName"
+	_, err = s.Login(nil, req1)
+	assert.Error(t, err, "User does not exists or password is invalid")
+
+	// Error - Invalid request password
+	req2 := &pb.LoginRequest{
+		UserName: resp.User.UserName,
+		Password: "",
+	}
+	_, err = s.Login(nil, req2)
+	assert.Error(t, err, "Invalid password length")
+	req2.Password = "resp.User.Password"
+	_, err = s.Login(nil, req2)
+	assert.Error(t, err, "User does not exists or password is invalid")
+}
+
+func Test_GetUserByID(t *testing.T) {
+	s := NewUserService()
+
+	_, err := s.GetUserByID(nil, nil)
+	assert.Error(t, err, "Not implemented")
+}
+
+func Test_RegisterUserServiceIntoGRPCServer(t *testing.T) {
+	assert.NotPanics(t, func() {
+		grpcServer := grpc.NewServer()
+		RegisterUserServiceIntoGRPCServer(grpcServer)
+	})
 }
